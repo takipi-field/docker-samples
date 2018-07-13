fa4d9c3dafc7c8eea4d6743a6111018382b54811
Parker Temple <parkertemple@OverOpsPTs-MacBook-Pro.local>
2018-07-13 11:45:22 -0700

    first commit

---
+++ agent/run.sh
@@ -0,0 +1,1 @@
+docker run -e "TAKIPI_COLLECTOR_HOST=127.0.0.1" -e "TAKIPI_COLLECTOR_PORT=6060" --net="host" -d agent
---
+++ collector/run.sh
@@ -0,0 +1,1 @@
+docker run -e "TAKIPI_LISTEN_PORT=6060" -p=6060:6060 -d collector
